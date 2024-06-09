98a646bfe81b7a3cf1dad0240295f1206c9b60a1
Mattias007 <mattiasmaasik@ametikool.ee>
2024-06-09 13:10:51 +0300

    asd

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -23,23 +23,23 @@ export async function logout() {
 }
 
 export async function graphData(){
-  const res = await fetch(`http://85.89.32.58/feed/data.json?id=52&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `)
+  const res = await fetch(`http://85.89.32.58/feed/data.json?id=52&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `,{ next: { revalidate: 3600 } })
   return await res.json()
 }
 
 export async function graphData2(){
-  const res = await fetch(`http://85.89.32.58/feed/data.json?id=53&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `)
+  const res = await fetch(`http://85.89.32.58/feed/data.json?id=53&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `,{ next: { revalidate: 3600 } })
   return await res.json()
 }
 
 
 export async function graphData3(){
-  const res = await fetch(`http://85.89.32.58/feed/data.json?id=60&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `)
+  const res = await fetch(`http://85.89.32.58/feed/data.json?id=60&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `,{ next: { revalidate: 3600 } })
   return await res.json()
 }
 
 export async function graphData4(){
-  const res = await fetch(`http://85.89.32.58/feed/data.json?id=61&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `)
+  const res = await fetch(`http://85.89.32.58/feed/data.json?id=61&start=-24 hour&end=now&interval=3600&average=0&timeformat=unix&skipmissing=0&limitinterval=0&delta=0&apikey=7bf4e9f0d76c7fd480b1eae9699aaac9 `,{ next: { revalidate: 3600 } })
   return await res.json()
 }
 
